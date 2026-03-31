b2ea6058521759f3b98403d039b966cdd6e38d44
teskleja <panjiariyojipang@gmail.com>
2026-03-31 20:08:37 +0700

    remove blog for static export

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 const withMDX = require("@next/mdx")();
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export", // 🔥 WAJIB
+
+  basePath: "/website-master", // 🔥 ganti sesuai nama repo lo
+  assetPrefix: "/website-master/",
+
   pageExtensions: ["js", "jsx", "mdx"],
+
   images: {
+    unoptimized: true, // 🔥 WAJIB untuk static export
     domains: [
-      "dl.airtable.com",
-      "v5.airtableusercontent.com",
       "assets-global.website-files.com",
       "www.notion.so",
       "notion.so",
@@ -13,7 +19,7 @@ const nextConfig = {
       "prod-files-secure",
     ],
   },
-  // Enable video loading from various sources
+
   async headers() {
     return [
       {
